a3e65243e33e57d55c5bc5f18aac0ccafc5106df
Jakub Grzegorczyk <boginow@gmail.com>
2022-06-10 18:05:21 +0200

    Asd

---
+++ KolokwiumPrzykladowe/Entities/MusicContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using KolokwiumPrzykladowe.Entities.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace KolokwiumPrzykladowe.Entities
+{
+    public class MusicContext : DbContext
+    {
+        public MusicContext()
+        {
+        }
+
+        public MusicContext(DbContextOptions<MusicContext> options) : base(options)
+        {
+
+        }
+
+
+        public virtual DbSet<Album> Albums { get; set; }
+        public virtual DbSet<Musican_Track> Musican_Tracks { get; set; }
+        public virtual DbSet<Musician> Musicians { get; set; }
+        public virtual DbSet<MusicLabel> MusicLabels { get; set; }
+        public virtual DbSet<Track> Tracks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(
+                typeof(TrackEfConfiguration).GetTypeInfo().Assembly);
+        }
+    }
+}
